840bcd004ed8687bc2e596583c639b12e18c200b
Kalaikumar <kalai.k.t@gmail.com>
2018-03-20 20:38:38 -0400

    project page update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { DataService } from '../data.service';
 import * as $ from 'jquery';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
@@ -16,7 +17,7 @@ export class ProjectsComponent implements OnInit {
   ngOnInit() {
     this.dataService.getProjects()
     .subscribe((res: Response)  => {
-      let company = '';
+      /*let company = '';
       let idx = 0;
       let projects = [];
       res.json().forEach((project) =>{
@@ -26,20 +27,21 @@ export class ProjectsComponent implements OnInit {
             projects[idx-1] = {"companyName": company, "projects":[] };
         }
         projects[idx-1].projects.push(project);
-      });
+      });*/
 
-      this.projects = projects;
+      this.projects = res.json();
     });
     setTimeout(
       function(){
         $('.project-item').mouseover(function(){
           let height = $(this).height() - $(this).find('.project-info').height();
-          $(this).find('.responsibilities').height(height - 60);
+          
+          if($(this).data('launchbtn'))
+            $(this).find('.responsibilities').height(height - 110);
+          else
+            $(this).find('.responsibilities').height(height - 60);
         });
-      },
-      200
-    );
-
+      }, 500);
   }
 
 }
